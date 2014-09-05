65dea52ac510b02c501a8ef94189dd44b03a5e57
bandi.shippuden <bandi.shippuden@gmail.com>
2014-09-05 19:53:28 +0000

    block iklan

---
+++ ads.js
@@ -0,0 +1,5 @@
+// block ads by Bandi Shippuden //
+// Maaf ya ikan saya block //
+// Bikin Greget se, nah kalo ini bukan iklan //
+// Mohon Infonya ya Sob, //
+// http://bandi-shippuden.blogspot.com //
